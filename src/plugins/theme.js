1e0b04f854654ec7fc83a68680b0ba6983bd2b45
YuzeTT <ziv1024@163.com>
2021-06-05 00:49:13 +0800

    首次提交

---
+++ src/plugins/theme.js
@@ -0,0 +1,13 @@
+export default {
+    dark: false,
+    themes: {
+        light: {
+            backgroundColor: '#FFFFFF',
+            'IDChip-bc': '#F4F4F6',
+        },
+        dark: {
+            backgroundColor: '#14181D',
+        }
+    }
+    
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,108 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <hello-world /> -->
+  <div class="pa-5">
+    <v-card elevation="0">
+      <v-row
+        align="center"
+        justify="space-around"
+        class="pa-2"
+      >
+        <v-btn
+          large
+          elevation="0"
+          icon
+          outlined
+          tile
+          style="border: 2px solid #EDEFF2;border-radius:15px"
+        >
+          <v-icon large>mdi-equal</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn
+          large
+          elevation="0"
+          icon
+          @click="switchDark()"
+        >
+          <v-icon>mdi-brightness-4 </v-icon>
+        </v-btn>
+      </v-row>
+    </v-card>
+    <div class="ma-6"></div>
+    <v-card elevation="0">
+      <div>
+        <span class="text-h3 font-weight-bold">Trading Net</span>
+        <div class="py-1"/>
+        <span class="text--disabled font-weight-medium">Welcome back, YuzeTT</span>
+        <v-chip class="ml-2 px-2 py-1" color="IDChip-bc" label>ID 0001</v-chip>
+      </div>
+    </v-card>
+    <div class="ma-6"></div>
+    <v-card elevation="0">
+      <v-row
+        align="start"
+        no-gutters
+        style="height: 150px;"
+      >
+        <v-col>
+          <v-card
+            elevation="0"
+            style="border: 2px solid #EDEFF2;border-radius:15px"
+            class="pa-4"
+          >
+            <v-icon
+              medium
+              color="green lighten-1"
+            >mdi-console-line</v-icon>
+            <div class="py-1"></div>
+            <span class="text-h4 font-weight-bold">126 ms</span>
+            <br>
+            <span class="text--disabled">编译耗时</span>
+          </v-card>
+        </v-col>
+        <div class="pa-1"></div>
+        <v-col>
+          <v-card
+            elevation="0"
+            style="border: 2px solid #EDEFF2;border-radius:15px"
+            class="pa-4"
+          >
+            <v-icon
+              medium
+              color="red"
+            >mdi-database-alert</v-icon>
+            <div class="py-1"></div>
+            <span class="text-h4 font-weight-bold">0 %</span>
+            <br>
+            <span class="text--disabled">数据库错误率</span>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style >
+body {
+  padding: 0;
+  margin: 0;
 }
+</style>
+
+<script>
+  // import HelloWorld from '../components/HelloWorld'
+  import theme from '../plugins/theme'
+  export default {
+    name: 'Home',
+    components: {
+      // HelloWorld,
+    },
+    data: () => ({ value: 1 }),
+    methods: {
+      switchDark:function() {
+        this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+        theme.dark = !theme.dark
+      }
+    }
+  }
 </script>
